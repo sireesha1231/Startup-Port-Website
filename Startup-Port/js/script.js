4b6c56d9144f554e171f7402174ac13edcf2964e
Sireesha Basamsetty <sireesha1994@gmail.com>
2017-11-22 17:01:58 -0600

    added files via upload

---
+++ Startup-Port/js/script.js
@@ -0,0 +1,27 @@
+AOS.init();
+$(window).on('load', function () {
+  AOS.refresh();
+});
+
+// Create the dropdown base
+$("<select />").appendTo("nav");
+
+// Create default option "Go to..."
+$("<option />", {
+   "selected": "selected",
+   "value"   : "",
+   "text"    : "Go to..."
+}).appendTo("nav select");
+
+// Populate dropdown with menu items
+$("nav a").each(function() {
+ var el = $(this);
+ $("<option />", {
+     "value"   : el.attr("href"),
+     "text"    : el.text()
+ }).appendTo("nav select");
+});
+
+$("nav select").change(function() {
+  window.location = $(this).find("option:selected").val();
+});
